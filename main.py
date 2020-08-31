445a36701d51c72051d0227031f7987b44359afb
arnet <you@example.com>
2020-08-31 00:55:48 -0300

    begin

---
+++ main.py
@@ -0,0 +1,27 @@
+import subprocess
+import re
+import json
+
+# capture sensors
+proc = subprocess.Popen("sensors", stdout = subprocess.PIPE, stderr= subprocess.PIPE)
+stdout, stderr = proc.communicate()
+
+# bytes to string
+temp_raw = stdout.decode("utf-8")
+#print(temp_raw)
+
+# parsing whit regex
+pattern = re.compile(r'(\w+):\s+(.\d+.\d+).C\b')
+matches = pattern.finditer(temp_raw)
+
+# make a dict
+core_temps = {}
+for match in matches:
+	core_temps[match.group(1)] = float(match.group(2))
+	
+# to json for send
+core_temps_json = json.dumps(core_temps)
+# for json to print
+print(json.dumps(core_temps, indent=4))
+
+# ready to post
