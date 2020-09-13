5eb4613c542cd8854b34b7ab2fb652fc1dcb12f0
arnet <you@example.com>
2020-09-12 22:26:54 -0300

    CoreTemp Class ok

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from coretemp import CoreTemps
 import requests
-
+import json
 
 URL = "https://httpbin.org/post"
 
@@ -10,14 +10,11 @@ ct = CoreTemps()
 # to json for send
 core_temps_json = ct.get_json()
 
-# for json to print
-
-
 # POST on server
 r = requests.post(URL, json= core_temps_json)
 
 if r.status_code == 200:
 	print("Posted on: " + URL)
 	print(r.text)
+	print(r.json()['json'])
 # insignifact change on testing branch
-
